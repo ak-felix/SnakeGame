cbea75a0e32f1f2004b8a2ebf116cb0bb9995733
ak-felix <47936818+ak-felix@users.noreply.github.com>
2020-09-27 21:00:18 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,58 +1,56 @@
-import random
-import curses
+from turtle import *
+from random import randrange
+from freegames import square, vector
 
-s = curses.initscr()
-curses.curs_set(0)
-sh, sw = s.getmaxyx()
-w = curses.newwin(sh, sw, 0, 0)
-w.keypad(1)
-w.timeout(100)
+food = vector(0,0)
+snake = [vector(10,0)]
+aim = vector(0,-10)
 
-snk_x = sw/4
-snk_y = sh/2
-snake = [
-    [snk_y, snk_x],
-    [snk_y, snk_x-1],
-    [snk_y, snk_x-2]
-]
+def change(x,y):
+    aim.x=x
+    aim.y=y
 
-food = [sh/2, sw/2]
-w.addch(int(food[0]), int(food[1]), curses.ACS_PI)
+def inside(head):
+    return -200 < head.x < 190 and -200 < head.y <190
 
-key = curses.KEY_RIGHT
+def move():
+    head = snake[-1].copy()
+    head.move(aim)
 
-while True:
-    next_key = w.getch()
-    key = key if next_key == -1 else next_key
+    if not inside(head) or head in snake:
+        square(head.x, head.y, 9, 'red')
+        update()
+        return
+    snake.append(head)
 
-    if snake[0][0] in [0, sh] or snake[0][1]  in [0, sw] or snake[0] in snake[1:]:
-        curses.endwin()
-        quit()
+    if head==food:
+        print('Snake:', len(snake))
+        food.x = randrange(-15,15)*10
+        food.y = randrange(-15,15)*10
 
-    new_head = [snake[0][0], snake[0][1]]
-
-    if key == curses.KEY_DOWN:
-        new_head[0] += 1
-    if key == curses.KEY_UP:
-        new_head[0] -= 1
-    if key == curses.KEY_LEFT:
-        new_head[1] -= 1
-    if key == curses.KEY_RIGHT:
-        new_head[1] += 1
-
-    snake.insert(0, new_head)
-
-    if snake[0] == food:
-        food = None
-        while food is None:
-            nf = [
-                random.randint(1, sh-1),
-                random.randint(1, sw-1)
-            ]
-            food = nf if nf not in snake else None
-        w.addch(food[0], food[1], curses.ACS_PI)
     else:
-        tail = snake.pop()
-        w.addch(int(tail[0]), int(tail[1]), ' ')
+        snake.pop(0)
 
-    w.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
+    clear()
+
+    for body in snake:
+        square(body.x, body.y, 9, 'black')
+
+    square(food.x, food.y, 9, 'green')
+    update()
+    ontimer(move, 100)
+
+setup(420,420,370,0)
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: change(10,0),'Right')
+onkey(lambda: change(-10,0), 'Left')
+onkey(lambda: change(0,10), 'Up')
+onkey(lambda: change(0,-10), 'Down')
+move()
+done()
+    
+    
+
+
